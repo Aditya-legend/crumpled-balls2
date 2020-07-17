eafbf9eac5231bed4f1d9523e2c936b1d7839db2
Aditya-legend <67062121+Aditya-legend@users.noreply.github.com>
2020-07-17 18:11:22 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,54 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+
+
+	Engine.run(engine);
+
+		}
+
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  
+  drawSprites();
+  var options={
+	isStatic:false,
+	restitution:0.3,
+	friction:0.5,
+	density:1.2
+
+}
+if(paper.body===dustbinbody){
+	groundObject.display();
+	dustbinObj.display();
+	paperObject.display();
+}
+if(paper.body==dustbinbody){
+	paperObject.display();
+	groundObject.display();
+	dustbinrObj.display();
+}
+}
+
+
